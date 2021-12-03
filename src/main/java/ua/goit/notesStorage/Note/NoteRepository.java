4c60b1f8652a43d1b5a338dd430b505543db87aa
oleksiikrutko <krutko.av@gmail.com>
2021-12-03 10:43:57 +0200

    refactored code #2 in NoteRepository

--- src/main/java/ua/goit/notesStorage/Note/NoteRepository.java
+++ src/main/java/ua/goit/notesStorage/Note/NoteRepository.java
@@ -1,7 +1,6 @@
 package ua.goit.notesStorage.Note;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,13 +9,12 @@ import java.util.UUID;
 @Repository
 public interface NoteRepository extends JpaRepository<Note, UUID> {
 
-    //@Query("SELECT u FROM #{#entityName} u WHERE u.name=?1")
     List<Note> findByName(String name);
 
-    @Query("SELECT c FROM Note c WHERE NOT (c.accessType='PRIVATE' AND c.author.id!=?1)")
-    List<Note> getListNotes(UUID uuid);
+//    @Query("SELECT c FROM Note c WHERE NOT (c.accessType='PRIVATE' AND c.author.id!=?1)")
+//    List<Note> getListNotes(UUID uuid);
 
-    @Query("SELECT c FROM Note c WHERE (c.author.id=?1)")
+//    @Query("SELECT c FROM Note c WHERE (c.author.id=?1)")
     List<Note> findByAuthor(UUID uuid);
 
 }
